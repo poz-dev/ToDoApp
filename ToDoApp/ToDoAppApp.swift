fa6226620a1a0b70e727244935fd46383189b661
Kresimir Ivanjko <tp8pbrzyr4@privaterelay.appleid.com>
2023-06-01 20:53:07 +0200

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Kresimir Ivanjko on 01.06.2023..
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
